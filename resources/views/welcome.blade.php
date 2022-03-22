e24cc693815a6c73dd0d3d523d7ae39385092c0a
Cristian García <cristianfernadogarciabernabe@gmail.com>
2022-03-22 17:42:32 -0600

    quitar cosas innecesarias

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{mix('css/app.css')}}">
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </head>
     <body>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
